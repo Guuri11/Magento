7fb882f9f116af84905a280256800aae0aa916d7
Guuri11 <sergio.gurillo11@gmail.com>
2020-03-11 16:35:28 +0100

    Added admin grid ui component (does not work correctly, I hope fix it once I watch the next udemy tutorial). Finished the first udemy tutorial.

--- app/code/UdemyMagento/DataBaseModule/Model/ResourceModel/AffiliateMember/Collection.php
+++ app/code/UdemyMagento/DataBaseModule/Model/ResourceModel/AffiliateMember/Collection.php
@@ -7,7 +7,6 @@ namespace UdemyMagento\DataBaseModule\Model\ResourceModel\AffiliateMember;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use UdemyMagento\DataBaseModule\Model\AffiliateMember;
 use UdemyMagento\DataBaseModule\Model\ResourceModel\AffiliateMember as AffiliateRM;
-
 class Collection extends AbstractCollection
 {
     protected function _construct()
