8b8ea3b6c3c05e52c533ce729072c93c27cd8a26
Guuri11 <sergio.gurillo11@gmail.com>
2020-03-12 16:03:09 +0100

    Created my own Command by my self, create a new item and get all the items or one of them

--- app/code/Mastering/SampleModule/Console/Command/GetItems.php
+++ app/code/Mastering/SampleModule/Console/Command/GetItems.php
@@ -7,11 +7,13 @@ namespace Mastering\SampleModule\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Mastering\SampleModule\Model\ItemFactory;
 
 
 class GetItems extends Command
 {
+    const ITEM_ID = 'id';
 
     protected $itemFactory;
 
@@ -24,17 +26,34 @@ class GetItems extends Command
     protected function configure()
     {
         $this->setName('mastering:item:get')
-            ->setDescription('the command prints all the items')
-            ->setAliases(['gi']);;
+            ->setDescription('the command prints all the items or one of them')
+            ->setAliases(['gi']);
+        $this->setDefinition([
+            new InputArgument(self::ITEM_ID,InputArgument::OPTIONAL,'Id of the item that you want to get',
+                null,null)
+        ]);
         parent::configure();
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     *
+     * Will return one|all item/s
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $items = $this->itemFactory->create();
-        $items = $items->getCollection()->getItems();
-        foreach ($items as $item){
-            $output->writeln('Item '.$item->getId().' is called '.$item->getName().'('.$item->getDescription().')');
+
+        if ($input->hasArgument(self::ITEM_ID)){
+            $item = $items->load($input->getArgument(self::ITEM_ID));
+            $item->isEmpty() ? $output->writeln('Item doesn\'t exist'):$output->writeln('Item '.$item->getId().' is called '.$item->getName().'('.$item->getDescription().')');
+        }else{
+            $items = $items->getCollection()->getItems();
+            foreach ($items as $item){
+                $output->writeln('Item '.$item->getId().' is called '.$item->getName().'('.$item->getDescription().')');
+            }
         }
     }
 
